ef74a91b0f9e3cfe2971d20eb03eaeb415f2edb4
MichaelServices <michaelmccraw52@gmail.com>
2024-09-20 20:00:46 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+player.onChat("Crash = True", function () {
+    for (let index = 0; index < 99999; index++) {
+        mobs.kill(
+        mobs.target(ALL_PLAYERS)
+        )
+        gameplay.timeSet(gameplay.time(DAY))
+        gameplay.timeSet(gameplay.time(NIGHT))
+        gameplay.title(mobs.target(ALL_PLAYERS), "MICHAEL SERVICES", "GET CRASHED BY MICHAEL SERVICES NO MORE GAME EZ")
+    }
+})
+player.say("The mod is on and enabled type Crash = True to start crash (Everyone can see this msg lol)")
